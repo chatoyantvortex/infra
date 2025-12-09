7b52bb53a41ecade1676e35e0a3c172e634802d8
amirtharajvellingiri5 <amirtharaj.v@outlook.com>
2025-12-09 10:34:41 +0530

    do not delete namespace

--- kubernetes_setup.tf
+++ kubernetes_setup.tf
@@ -1,5 +1,15 @@
 resource "kubernetes_namespace" "apps" {
   metadata {
     name = "apps"
+    labels = {
+      managed = "terraform"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+      metadata[0].annotations,
+    ]
   }
 }
