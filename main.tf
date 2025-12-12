425f819345b51f66adc52504dce290828e5a1c4d
amirtharajvellingiri5 <amirtharaj.v@outlook.com>
2025-12-12 09:24:14 +0530

    remove duplicate

--- main.tf
+++ main.tf
@@ -7,17 +7,3 @@ module "docker_apps" {
     docker = docker
   }
 }
-
-resource "kubernetes_namespace" "apps" {
-  metadata {
-    name = "apps"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      metadata,
-    ]
-  }
-}
-
